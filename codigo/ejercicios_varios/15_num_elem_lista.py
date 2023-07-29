b528aaee3fb2d4900467848a02cc0d775d64a9e9
Jose Ojeda <josecodetech@gmail.com>
2023-07-29 08:48:44 +0200

    codigo ejemplo

---
+++ codigo/ejercicios_varios/15_num_elem_lista.py
@@ -0,0 +1,4 @@
+#¿Cómo contar la cantidad de elementos en una lista?
+lista = [1, 2, 3, 4, 5]
+cantidad = len(lista)
+print(cantidad) 
---
+++ codigo/ejercicios_varios/16_encontrar_elemento_lista.py
@@ -0,0 +1,7 @@
+#¿Cómo encontrar un elemento en una lista?
+lista = [1, 2, 3, 4, 5]
+elemento = 3
+if elemento in lista:
+    print("El elemento está en la lista")
+else:
+    print("El elemento no está en la lista")
