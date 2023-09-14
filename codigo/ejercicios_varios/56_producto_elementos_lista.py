bbb4186144b9db84f45c4ecff95e0b34248af317
Jose Ojeda <josecodetech@gmail.com>
2023-09-14 07:19:37 +0200

    varios

---
+++ codigo/ejercicios_varios/56_producto_elementos_lista.py
@@ -0,0 +1,6 @@
+# ¿Cómo calcular el producto de los elementos de una lista?
+import math
+lista = [1, 2, 3, 4, 5]
+producto = math.prod(lista)
+print(lista)
+print(producto)
---
+++ codigo/ejercicios_varios/57_cadena_alfanumerica.py
@@ -0,0 +1,5 @@
+# ¿Cómo verificar si una cadena contiene solo caracteres alfanuméricos?
+cadena = "Hola123"
+es_alfanumerica = cadena.isalnum()
+print(cadena)
+print(es_alfanumerica)
